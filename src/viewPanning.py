56865b9254a1da5acb119e1b77022f1a01e2194e
chen yu qiang <a@example.com>
2021-08-19 22:33:21 +0800

    W

--- src/viewPanning.py
+++ src/viewPanning.py
@@ -51,7 +51,10 @@ def d_v(pk,ci):
     return max(abs(tpk[0]),abs(tpk[1]),abs(tpk[2]),abs(tpk[3]))
 
 def C_s(pk,ci):
-    return np.exp((-1.0)*RHO*d_v(pk,ci)*d_v(pk,ci))
+    if d_v(pk,ci)>100:
+        return 0
+    else:
+        return np.exp((-1.0)*RHO*d_v(pk,ci))
 
 def whatPartition(pk,ci):
     cpk=worldFrame2CameraFrame(pk,ci)
@@ -157,83 +160,231 @@ def mut_point(ci,taskPoint):
     return all_delta_ci
 
 
-def v_y(x,y,th,l=-0.1):
+def v_y(x,y,th,l=-0.5):
     xx=x+np.cos(th+np.pi*0.5)*l
     yy=y+np.sin(th+np.pi*0.5)*l
     return [x,xx],[y,yy]
 
 if __name__ == '__main__':
-    taskPoint[0]=[0.0,0.0,0.0,np.pi/2]
-    taskPoint[1]=[0.0,0.5,0.0,np.pi/2]
+
+    from geometry_msgs.msg import Twist
+    import matplotlib.pyplot as plt
+
+    TASKPOINT_NUM=2
+    taskPoint=[[0.0,0.0,0.0,0.0] for i in range(TASKPOINT_NUM)]
+
+    # Case board
+    # taskPoint[0]=[0.0,0.0,0.0,np.pi/2]
+
+    # Case 1
+    taskPoint[0]=[0.0,0.65,0.0,np.pi/2]
+    taskPoint[1]=[0.0,-0.8,0.0,np.pi*0.5]
+
+    # Case 2
+    # taskPoint[0]=[0.0,0.65,0.0,np.pi/2]
+    # taskPoint[1]=[0.0,-0.8,0.0,np.pi*2.0/3]
+
+    # Case 3
+    # taskPoint[0]=[0.0,0.65,0.0,np.pi/2]
+    # taskPoint[1]=[0.52,-0.8,0.0,np.pi*0.75]
+
+
+    targetSet=[Twist() for i in range(TASKPOINT_NUM)]
+
+    for i in range(len(taskPoint)):
+        targetSet[i].linear.x=taskPoint[i][0]
+        targetSet[i].linear.y=taskPoint[i][1]
+        targetSet[i].linear.z=taskPoint[i][2]
+        targetSet[i].angular.z=taskPoint[i][3]
+
     ci=[1.0,1.0,0.0,np.pi]
     it_length=1
 
-    for i in range(10000):
+    overall_per=[0 for i in range(1000)]
+    t1_per=[0 for i in range(1000)]
+    t2_per=[0 for i in range(1000)]
+    trj_x=[0 for i in range(1000)]
+    trj_y=[0 for i in range(1000)]
+    tck1_1=[0 for i in range(1000)]
+    tck1_2=[0 for i in range(1000)]
+    tck1_3=[0 for i in range(1000)]
+    tck1_4=[0 for i in range(1000)]
+    tck2_1=[0 for i in range(1000)]
+    tck2_2=[0 for i in range(1000)]
+    tck2_3=[0 for i in range(1000)]
+    tck2_4=[0 for i in range(1000)]
+
+    board_color=["tab:orange","tab:green"]
+    # for i in range(len(taskPoint)):
+    #     plt.scatter(targetSet[i].linear.x,targetSet[i].linear.y,color=board_color[i])
+    #     [x,xx],[y,yy]=v_y(targetSet[i].linear.x,targetSet[i].linear.y,targetSet[i].angular.z)
+    #     plt.plot([x,xx],[y,yy],color=board_color[i])
+
+
+    for i in range(1000):
         delta_ci=mut_point(ci,taskPoint)
         # print(delta_ci)
         ci[0]=ci[0]+it_length*delta_ci[0]
         ci[1]=ci[1]+it_length*delta_ci[1]
         ci[2]=ci[2]+it_length*delta_ci[2]
         ci[3]=ci[3]+it_length*0.1*delta_ci[3]
-        length=delta_ci[0]*delta_ci[0]+delta_ci[1]*delta_ci[1]+delta_ci[2]*delta_ci[2]+delta_ci[3]*delta_ci[3]*0.01
         it_length=it_length*0.999
+        res=Twist()
+        res.linear.x=ci[0]
+        res.linear.y=ci[1]
+        res.linear.z=ci[2]
+        res.angular.z=ci[3]
+        t1_per[i]=C_s(taskPoint[0],ci)
+        # t2_per[i]=C_s(taskPoint[1],ci)
+        overall_per[i]=t1_per[i]+t2_per[i]
+
+
+
+        # for trajectory =====================================
+        # trj_x[i]=ci[0]
+        # trj_y[i]=ci[1]
+        # plt.scatter(res.linear.x,res.linear.y,color="tab:blue")
+        # [x,xx],[y,yy]=v_y(res.linear.x,res.linear.y,res.angular.z,0.3)
+        # plt.plot([x,xx],[y,yy],color="tab:blue")    
+        # ===========================================================        
+
+
+
+        # for animation=====================================================================
+        # plt.scatter(target1.linear.x,target1.linear.y)
+        # [x,xx],[y,yy]=v_y(target1.linear.x,target1.linear.y,target1.angular.z)
+        # plt.plot([x,xx],[y,yy])
+        # plt.scatter(target2.linear.x,target2.linear.y)
+        # [x,xx],[y,yy]=v_y(target2.linear.x,target2.linear.y,target2.angular.z)
+        # plt.plot([x,xx],[y,yy])
+        # plt.scatter(res.linear.x,res.linear.y)
+        # [x,xx],[y,yy]=v_y(res.linear.x,res.linear.y,res.angular.z,0.3)
+        # plt.plot([x,xx],[y,yy])    
+        # plt.axis([-1,5,-1,2])
+        # plt.grid(True)
+
+        # plt.draw()
+        # plt.pause(0.1)
+        # plt.clf()
+        # =================================================================================
+
+        cpk=worldFrame2CameraFrame(taskPoint[0],ci)
+        tpk=ciSpace2tiSpace(cpk)
+
+        tck1_1[i]=tpk[0]
+        tck1_2[i]=tpk[1]
+        tck1_3[i]=tpk[2]
+        tck1_4[i]=tpk[3]
+
+        cpk=worldFrame2CameraFrame(taskPoint[1],ci)
+        tpk=ciSpace2tiSpace(cpk)
+
+        tck2_1[i]=tpk[0]
+        tck2_2[i]=tpk[1]
+        tck2_3[i]=tpk[2]
+        tck2_4[i]=tpk[3]
+
         print("ci",ci,i)
-    
-    cpk=worldFrame2CameraFrame(taskPoint[0],ci)
-    tpk=ciSpace2tiSpace(cpk)
-    print(tpk)
-    cpk=worldFrame2CameraFrame(taskPoint[1],ci)
-    tpk=ciSpace2tiSpace(cpk)
-    print(tpk)
 
+    # plt.plot(trj_x,trj_y,color="b")
+    # plt.axis([-1,5,-2,2])
+    # plt.grid(True)
+    # plt.xlabel("X (m)")
+    # plt.ylabel("Y (m)")
+    # plt.show()
 
-    from geometry_msgs.msg import Twist
-
-    target1=Twist()
-    target1.linear.x=taskPoint[0][0]
-    target1.linear.y=taskPoint[0][1]
-    target1.linear.z=taskPoint[0][2]
-    target1.angular.z=taskPoint[0][3]
-    target2=Twist()
-    target2.linear.x=taskPoint[1][0]
-    target2.linear.y=taskPoint[1][1]
-    target2.linear.z=taskPoint[1][2]
-    target2.angular.z=taskPoint[1][3]
-    
-    
-    res=Twist()
-    res.linear.x=ci[0]
-    res.linear.y=ci[1]
-    res.linear.z=ci[2]
-    res.angular.z=ci[3]
-
-    import matplotlib.pyplot as plt
-    plt.scatter(target1.linear.x,target1.linear.y)
-    [x,xx],[y,yy]=v_y(target1.linear.x,target1.linear.y,target1.angular.z)
-    plt.plot([x,xx],[y,yy])
-    plt.scatter(target2.linear.x,target2.linear.y)
-    [x,xx],[y,yy]=v_y(target2.linear.x,target2.linear.y,target2.angular.z)
-    plt.plot([x,xx],[y,yy])
-    # plt.scatter((target2.linear.x+target1.linear.x)*0.5,(target2.linear.y+target1.linear.y)*0.5)
-    # plt.plot([target1.linear.x,target2.linear.x],[target1.linear.y,target2.linear.y])
-    plt.scatter(res.linear.x,res.linear.y)
-    [x,xx],[y,yy]=v_y(res.linear.x,res.linear.y,res.angular.z,0.3)
-    plt.plot([x,xx],[y,yy])    
-    # plt.axis([-3,3,-3,3])
+    plt.plot(tck1_1)
+    plt.plot(tck1_2)
+    plt.plot(tck1_3)
+    plt.plot(tck1_4)
     plt.grid(True)
-
+    plt.xlabel("Number of iterations")
+    plt.ylabel(" (m)")
+    plt.axis([0,1000,0,2])
+    plt.legend(["tp_xk","tp_yk","tp_zk","tp_thk"])
     plt.show()
 
-    x_list=np.linspace(1,5,41)
-    y_list=np.linspace(-1,2,31)
-    th_list=np.linspace(0,np.pi,37)
-    the_MAX=0
-    for i in range(len(x_list)):
-        for j in range(len(y_list)):
-            for k in range(len(th_list)):
-                ci=[x_list[i],y_list[j],0,th_list[k]]
-                pk1=taskPoint[0]
-                pk2=taskPoint[1]
-                if C_s(pk1,ci)+C_s(pk2,ci)>the_MAX:
-                    the_MAX=C_s(pk1,ci)+C_s(pk2,ci)
-                    print(x_list[i],y_list[j],th_list[k],C_s(pk1,ci)+C_s(pk2,ci))
+
+    plt.plot(tck2_1)
+    plt.plot(tck2_2)
+    plt.plot(tck2_3)
+    plt.plot(tck2_4)
+    plt.grid(True)
+    plt.xlabel("Number of iterations")
+    plt.ylabel(" (m)")
+    plt.axis([0,1000,0,2])
+    plt.legend(["tp_xk","tp_yk","tp_zk","tp_thk"])
+    plt.show()
+
+
+
+
+    # plt.figure()
+    # plt.plot(t1_per)
+    # plt.xlabel("Number of iterations")
+    # plt.ylabel("Performance")
+    # plt.title("Target 51")
+    # plt.grid(True)
+    # plt.axis([0,1000,0,2])
+    # plt.show()
+
+
+    # plt.figure()
+    # plt.plot(t2_per)
+    # plt.xlabel("Number of iterations")
+    # plt.ylabel("Performance")
+    # plt.title("Target 52")
+    # plt.grid(True)
+    # plt.axis([0,1000,0,2])
+    # plt.show()
+    
+    # plt.figure()
+    # plt.plot(overall_per)
+    # plt.xlabel("Number of iterations")
+    # plt.ylabel("Performance")
+    # plt.title("Overall")
+    # plt.grid(True)
+    # plt.axis([0,1000,0,2])
+    # plt.show()
+
+
+
+
+
+
+
+    # for contourf=======================================================
+    # x_list=np.linspace(-1,5,401)
+    # y_list=np.linspace(-2,2,401)
+
+    # def f(x,y):
+    #     C_s_sum=0
+    #     _ci=[x,y,0,ci[3]]
+    #     for i in range(len(taskPoint)):
+    #         C_s_sum=C_s_sum+C_s(taskPoint[i],_ci)
+    #         # if C_s(taskPoint[i],_ci)==0:
+    #         #     return 0
+    #     return C_s_sum
+
+
+    # Z=[[0 for i in range(len(x_list))]for j in range(len(y_list))]
+    # for i in range(len(x_list)):
+    #      for j in range(len(y_list)):
+    #          Z[j][i]=f(x_list[i],y_list[j])
+
+
+    # plt.contourf(x_list, y_list, Z,100,cmap='jet')
+    # plt.colorbar()    
+    # plt.axis([-1,5,-2,2])
+    # plt.scatter(res.linear.x,res.linear.y)
+    # [x,xx],[y,yy]=v_y(res.linear.x,res.linear.y,res.angular.z,0.3)
+    # plt.plot([x,xx],[y,yy])    
+
+    # for i in range(len(taskPoint)):
+    #     plt.scatter(targetSet[i].linear.x,targetSet[i].linear.y,color=board_color[i])
+    #     [x,xx],[y,yy]=v_y(targetSet[i].linear.x,targetSet[i].linear.y,targetSet[i].angular.z)
+    #     plt.plot([x,xx],[y,yy],color=board_color[i])
+
+    # plt.xlabel("X (m)")
+    # plt.ylabel("Y (m)")
+    # plt.show()
